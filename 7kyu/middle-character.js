843bcf52a6e4d0a08f5ce0e33840e6dd8a5a80cc
yuli-r <iuliana.rotaru234@gmail.com>
2023-01-21 19:30:04 +0000

    Complete middle character kata

---
+++ 7kyu/middle-character.js
@@ -0,0 +1,21 @@
+/*You are going to be given a word. Your job is to return the 
+middle character of the word. If the word's length is odd, 
+return the middle character. If the word's length is even, 
+return the middle 2 characters.
+
+#Examples:
+
+Kata.getMiddle("test") should return "es"
+
+Kata.getMiddle("testing") should return "t"
+
+Kata.getMiddle("middle") should return "dd"
+
+Kata.getMiddle("A") should return "A"*/
+
+function getMiddle(s) {
+  let middle = s.length / 2;
+  return s.length % 2
+    ? s.charAt(Math.floor(middle))
+    : s.slice(middle - 1, middle + 1);
+}
