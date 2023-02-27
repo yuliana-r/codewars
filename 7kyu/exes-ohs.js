793a52c38e8fa46edffc385aef0e08335e69d74b
yuli-r <iuliana.rotaru234@gmail.com>
2023-02-27 15:28:26 +0000

    Complete 2 fighters 1 winner kata

--- 7kyu/exes-ohs.js
+++ 7kyu/exes-ohs.js
@@ -1,5 +1,5 @@
-/*Check to see if a string has the same amount of 'x's and 'o's. 
-The method must return a boolean and be case insensitive. 
+/* Check to see if a string has the same amount of 'x's and 'o's.
+The method must return a boolean and be case insensitive.
 The string can contain any char.
 
 Examples input/output:
@@ -11,9 +11,9 @@ XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
 XO("zzoo") => false */
 
 function XO(str) {
-  const arr = str.toLowerCase().split("");
+  const arr = str.toLowerCase().split('');
   return (
-    arr.filter((char) => char === "o").length ===
-    arr.filter((char) => char === "x").length
+    arr.filter((char) => char === 'o').length
+    === arr.filter((char) => char === 'x').length
   );
 }
