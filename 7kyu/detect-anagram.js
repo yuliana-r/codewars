4d3ab1ff2a5553ac165cf2d94481034df2cd3a69
yuli-r <iuliana.rotaru234@gmail.com>
2023-02-28 13:09:50 +0000

    Complete detect anagram kata

---
+++ 7kyu/detect-anagram.js
@@ -0,0 +1,14 @@
+/* Complete the function to return true if the two arguments given are
+anagrams of each other; return false otherwise.
+
+Examples
+"foefet" is an anagram of "toffee"
+
+"Buckethead" is an anagram of "DeathCubeK" */
+
+const isAnagram = function (test, original) {
+  return test.toLowerCase().split('').sort().join('') === original.toLowerCase().split('').sort().join('')
+    .toLowerCase();
+};
+
+console.log(isAnagram('Buckethead', 'DeathCubeK'));
