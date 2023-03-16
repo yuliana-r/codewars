243c6d02bef2d48917df96e9f9a13dd47bb65cd3
yuli-r <iuliana.rotaru234@gmail.com>
2023-03-16 15:23:59 +0000

    Complete valid parantheses kata

---
+++ 7kyu/valid-parantheses.js
@@ -0,0 +1,46 @@
+/*
+Write a function that takes a string of parentheses, and determines if the order of
+the parentheses is valid. The function should return true if the string is valid,
+and false if it's invalid.
+
+Examples
+"()"              =>  true
+")(()))"          =>  false
+"("               =>  false
+"(())((()())())"  =>  true
+Constraints
+0 <= length of input <= 100
+
+All inputs will be strings, consisting only of characters ( and ).
+Empty strings are considered balanced (and therefore valid), and will be tested.
+For languages with mutable strings, the inputs should not be mutated.
+
+Protip: If you are trying to figure out why a string of parentheses is invalid,
+paste the parentheses into the code editor, and let the code highlighting show you!
+*/
+
+function validParentheses(parenStr) {
+  const stack = [];
+
+  for (let i = 0; i < parenStr.length; i++) {
+    if (parenStr[i] === '(') {
+      stack.push(parenStr[i]);
+    } else if (stack.length > 0 && parenStr[i] === ')') {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+  return (stack.length === 0);
+}
+
+function validParentheses2(parenStr) {
+  let count = 0;
+
+  for (let i = 0; i < parenStr.length; i++) {
+    if (parenStr[i] === '(') { count++; } else { count--; }
+    if (count < 0) return false;
+  }
+
+  return count === 0;
+}
--- 8kyu/abbrev-name.js
+++ 8kyu/abbrev-name.js
@@ -1,4 +1,4 @@
-/*Write a function to convert a name into initials. 
+/* Write a function to convert a name into initials.
 This kata strictly takes two words with one space in between them.
 
 The output should be two capital letters with a dot separating them.
@@ -10,8 +10,8 @@ Sam Harris => S.H
 patrick feeney => P.F */
 
 function abbrevName(name) {
-  let newName = name.toUpperCase().split(" ");
-  return newName[0][0] + "." + newName[1][0];
+  const newName = name.toUpperCase().split(' ');
+  return `${newName[0][0]}.${newName[1][0]}`;
 }
 
-console.log(abbrevName("Sam Harris"));
+console.log(abbrevName('Sam Harris'));
