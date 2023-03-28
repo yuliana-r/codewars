7f534e5ed177ad5e307f36e081df1894df9e2240
yuli-r <iuliana.rotaru234@gmail.com>
2023-03-28 17:27:06 +0100

    Complete give me a diamond kata

--- 6kyu/build-tower.js
+++ 6kyu/build-tower.js
@@ -1,34 +1,34 @@
-/* Build a pyramid-shaped tower, as an array/list of strings, 
-given a positive integer number of floors. A tower block is 
+/* Build a pyramid-shaped tower, as an array/list of strings,
+given a positive integer number of floors. A tower block is
 represented with "*" character.
 
 For example, a tower with 3 floors looks like this:
 
 [
   "  *  ",
-  " *** ", 
+  " *** ",
   "*****"
 ]
 And a tower with 6 floors looks like this:
 
 [
-  "     *     ", 
-  "    ***    ", 
-  "   *****   ", 
-  "  *******  ", 
-  " ********* ", 
+  "     *     ",
+  "    ***    ",
+  "   *****   ",
+  "  *******  ",
+  " ********* ",
   "***********"
 ]
 */
 
 function towerBuilder(nFloors) {
-  let space,
-    star,
-    tower = [];
+  let space;
+  let star;
+  const tower = [];
 
   for (let i = 1; i <= nFloors; i++) {
-    space = " ".repeat(nFloors - i);
-    star = "*".repeat(2 * i - 1);
+    space = ' '.repeat(nFloors - i);
+    star = '*'.repeat(2 * i - 1);
     tower.push(`${space}${star}${space}`);
   }
   return tower;
